4d9dfd76cdfb846c789690471d93c4265478396e
mish0908 <mish0908@gmail.com>
2025-06-19 17:30:07 -0400

    done

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'michellezhu'
+socketio = SocketIO(app)
 
 # Welcome/Home page route
 @app.route('/')
 def index():
-    return render_template('welcome.html')
+    return render_template('welcome.html', active_page='home')
 
 # Birthday card route
 @app.route('/birthday_card', methods=['GET', 'POST'])
 def birthday_card():
-    return render_template('card.html')
+    return render_template('card.html', active_page='card')
 
 # Photo album route
 @app.route('/photo_album')
 def photo_album():
-    return render_template('photo_album.html')
+    return render_template('photo_album.html', active_page='相簿')
+
+# Chat route
+@app.route('/chat')
+def chat():
+    return render_template('chat.html', active_page='chat')
+
+# SocketIO event handlers
+@socketio.on('message')
+def handle_message(data):
+    emit('message', data, broadcast=True)
+
+@socketio.on('join')
+def handle_join(data):
+    emit('user_joined', data, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
